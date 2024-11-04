c5d6ed9fa64ff4cca2ef46fa418a683486f3dfe9
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2024-11-04 14:18:45 -0300

    Concluindo Service Acao Nota Fiscal

--- Services/AcaoNotaFiscalService.cs
+++ Services/AcaoNotaFiscalService.cs
@@ -1,4 +1,6 @@
+using Microsoft.EntityFrameworkCore;
 using Projeto_Transportadora_MVC.Context;
+using Projeto_Transportadora_MVC.Models;
 
 namespace Projeto_Transportadora_MVC.Services
 {
@@ -11,6 +13,33 @@ namespace Projeto_Transportadora_MVC.Services
             _context = context;
         }
 
-        
+        public async Task<IEnumerable<AcaoNotaFiscal>> ObterNotasNaoFechadasAsync()
+        {
+            return await _context.AcoesNotaFiscal
+                .Where(a => a.TipoAcao != Enums.TipoAcao.SaidaNoFechamento)
+                .Include(nf => nf.NotaFiscal)
+                .Include(c => c.Caminhao)
+                .ToListAsync();
+        }
+
+        public async Task<AcaoNotaFiscal> ObterPorIdAsync(int id)
+        {
+            return await _context.AcoesNotaFiscal
+                .Include(nf => nf.NotaFiscal)
+                .Include(c => c.Caminhao)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+
+        public async Task CriarAcaoAsync(AcaoNotaFiscal acaoNotaFiscal)
+        {
+            await _context.AcoesNotaFiscal.AddAsync(acaoNotaFiscal);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AtualizarAcaoAsync(AcaoNotaFiscal acaoNotaFiscal)
+        {
+            _context.AcoesNotaFiscal.Update(acaoNotaFiscal);
+            await _context.SaveChangesAsync();
+        }
     }
 }
