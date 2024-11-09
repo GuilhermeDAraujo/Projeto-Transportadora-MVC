c23bee46cad12e58e3708ab2dbde737b79ad63d1
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2024-11-09 19:55:41 -0300

    Concluindo melhorias da importacao de notas fiscais por excel

--- Services/ImportarExcelService.cs
+++ Services/ImportarExcelService.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.SignalR.Protocol;
 using Microsoft.EntityFrameworkCore;
 using OfficeOpenXml;
 using Projeto_Transportadora_MVC.Context;
+using Projeto_Transportadora_MVC.Enums;
 using Projeto_Transportadora_MVC.Models;
 
 namespace Projeto_Transportadora_MVC.Services
@@ -16,16 +18,22 @@ namespace Projeto_Transportadora_MVC.Services
 
         public async Task<List<NotaFiscal>> ImportarNotasFiscais(Stream arquivoExcel)
         {
+            var notasFiscais = ProcessarArquivoExcel(arquivoExcel);
+            await SalvarNotasEAssociarAcoes(notasFiscais);
+            return notasFiscais;
+        }
+
+        private List<NotaFiscal> ProcessarArquivoExcel(Stream arquivoExcel)
+        {
             ExcelPackage.LicenseContext = LicenseContext.Commercial;
-            List<NotaFiscal> notasFiscais = new List<NotaFiscal>();
-            List<AcaoNotaFiscal> acaoNotaFiscals = new List<AcaoNotaFiscal>();
+            List<NotaFiscal> notasFisicais = new List<NotaFiscal>();
 
             using (var package = new ExcelPackage(arquivoExcel))
             {
                 var planilha = package.Workbook.Worksheets[0];
 
                 if (planilha == null || planilha.Dimension == null || planilha.Dimension.End.Row < 2)
-                    throw new Exception("A planilha está vazia ou não possui dados.");
+                    throw new Exception("A planilha está vazia ou não possui dados");
 
                 for (int linha = 2; linha <= planilha.Dimension.End.Row; linha++)
                 {
@@ -35,48 +43,53 @@ namespace Projeto_Transportadora_MVC.Services
                         NomeCliente = planilha.Cells[linha, 2].Text,
                         EnderecoFaturado = planilha.Cells[linha, 3].Text,
                         DataDoFaturamento = DateTime.Parse(planilha.Cells[linha, 4].Text),
-                        NumeroDaCarga = int.Parse(planilha.Cells[linha, 6].Text)
+                        NumeroDaCarga = int.Parse(planilha.Cells[linha, 5].Text)
                     };
 
                     if (_context.NotasFiscais.Any(nf => nf.NumeroNotaFiscal == notaFiscal.NumeroNotaFiscal))
-                        throw new InvalidOperationException($"A nota fiscal {notaFiscal.NumeroNotaFiscal} já existe.");
+                        throw new InvalidOperationException($"A Nota Fiscal {notaFiscal.NumeroNotaFiscal} já existe");
 
-                    notasFiscais.Add(notaFiscal);
-
-                    acaoNotaFiscals.Add(new AcaoNotaFiscal
-                    {
-                        TipoAcao = Enums.TipoAcao.Entrada,
-                        DataDaAcao = DateTime.Now.Date,
-                        Descriacao = "Entrada da Nota Fiscal e aguardando ação",
-                        NotaFiscalId = notaFiscal.Id,
-                        CaminhaoId = 1,
-                        DataAgendada = null,
-                        StatusAgendamento = null
-                    });
+                    if(_context.NotasFiscais.Any(nf => nf.NomeCliente == null))
+                        throw new InvalidOperationException($"A Nota Fiscal {notaFiscal.NumeroNotaFiscal} não contém Nome do Cliente");
+                    notasFisicais.Add(notaFiscal);
                 }
             }
-            SalvarNotasFiscais(notasFiscais, acaoNotaFiscals);
-            return notasFiscais;
+            return notasFisicais;
         }
 
-        private void SalvarNotasFiscais(List<NotaFiscal> notasFiscais, List<AcaoNotaFiscal> acaoNotaFiscals)
+        private async Task SalvarNotasEAssociarAcoes(List<NotaFiscal> notasFiscais)
         {
-            using (var transaction = _context.Database.BeginTransaction())
+            using (var transaction = await _context.Database.BeginTransactionAsync())
             {
                 try
                 {
                     _context.NotasFiscais.AddRange(notasFiscais);
-                    _context.SaveChanges();
+                    await _context.SaveChangesAsync();
 
-                    _context.AcoesNotaFiscal.AddRange(acaoNotaFiscals);
-                    _context.SaveChanges();
+                    var acoesNotaFiscal = new List<AcaoNotaFiscal>();
+                    foreach (var notaFiscal in notasFiscais)
+                    {
+                        acoesNotaFiscal.Add(new AcaoNotaFiscal
+                        {
+                            TipoAcao = TipoAcao.Entrada,
+                            DataDaAcao = DateTime.Now.Date,
+                            Descriacao = "Entrada da Nota Fiscal e aguardando ação",
+                            NotaFiscalId = notaFiscal.Id,
+                            CaminhaoId = 1,
+                            DataAgendada = null,
+                            StatusAgendamento = null
+                        });
+                    }
 
-                    transaction.Commit();
+                    await _context.AcoesNotaFiscal.AddRangeAsync(acoesNotaFiscal);
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
                 }
                 catch (DbUpdateException ex)
                 {
-                    transaction.Rollback();
-                    throw new Exception("Erro ao salvar as notas fiscais", ex);
+                    await transaction.RollbackAsync();
+                    throw new Exception("Erro ao salvar as notas fiscais e ações", ex);
                 }
             }
         }
--- obj/Debug/net8.0/Projeto Transportadora MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto Transportadora MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Transportadora MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ba135d10ba00804d95e6346b15477218733cbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a498b0fc6a3ed214a42212aa6bcb283913c4e20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Transportadora MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Transportadora MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
